571111ae2c73a96593fd243bddbe47233ca4e184
PENWIN <162509565+ManuCaneva@users.noreply.github.com>
2025-09-16 22:18:53 -0300

    Add files via upload

---
+++ hospitales-data.sql
@@ -0,0 +1,24 @@
+INSERT INTO `HOSPITALES` (`direccion`,`correo_e`,`telefono`,`nombre`)
+VALUES
+  ("Ap #191-4727 Fusce St.","elit.pretium.et@google.com","(513) 918-8164","HospitalPascale"),
+  ("797-8681 Vitae, Ave","lacinia@aol.couk","1-381-344-3976","HospitalDominique"),
+  ("145-4644 Amet, St.","tincidunt@google.couk","(655) 134-1805","HospitalNero"),
+  ("681-9998 Luctus, Ave","faucibus@hotmail.couk","(879) 477-1326","HospitalWilliam"),
+  ("5423 Ullamcorper Street","nibh.sit.amet@icloud.com","(722) 525-6374","HospitalYuri"),
+  ("P.O. Box 927, 7780 Dui. Ave","montes.nascetur@outlook.edu","(918) 347-4836","HospitalKennedy"),
+  ("334-8800 Mauris St.","tempor.arcu@icloud.com","1-852-528-6888","HospitalWhilemina"),
+  ("P.O. Box 332, 4690 Tortor. St.","non.arcu@aol.org","(457) 490-0140","HospitalAnika"),
+  ("Ap #291-795 At St.","lobortis.augue.scelerisque@google.org","(884) 818-2954","HospitalEcho"),
+  ("Ap #129-3498 Eget Road","neque.nullam.ut@google.couk","1-470-819-8844","HospitalCecilia");
+INSERT INTO `HOSPITALES` (`direccion`,`correo_e`,`telefono`,`nombre`)
+VALUES
+  ("5224 Nullam St.","adipiscing.lacus@icloud.ca","1-633-756-9839","HospitalEmerald"),
+  ("394-6592 Morbi Avenue","nonummy.ac@hotmail.net","1-426-874-6286","HospitalAlden"),
+  ("P.O. Box 282, 1905 Malesuada Avenue","mauris.sapien@hotmail.edu","(556) 887-8487","HospitalAaron"),
+  ("512 Purus Avenue","hendrerit.donec@protonmail.com","(275) 548-8886","HospitalDennis"),
+  ("Ap #481-6245 Elementum, Street","nec.imperdiet@google.com","1-668-858-2238","HospitalDoris"),
+  ("Ap #410-5957 Massa. St.","mauris@outlook.com","(923) 322-1417","HospitalClare"),
+  ("9611 Mollis Ave","leo.in@icloud.ca","1-737-549-6822","HospitalNerea"),
+  ("339-5499 Risus Rd.","laoreet.ipsum.curabitur@yahoo.net","(890) 744-1037","HospitalKyra"),
+  ("P.O. Box 693, 4438 Quis Road","et.tristique@outlook.ca","1-655-742-8313","HospitalRoth"),
+  ("P.O. Box 702, 6319 Sociis St.","fringilla.purus@google.com","(148) 874-9337","HospitalNehru");
