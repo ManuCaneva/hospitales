fa049e285aed4f490d2d20ca4f71a96d8b692a16
GOYA <francomanuc@gmail.com>
2025-09-18 22:49:04 +0000

    bomboclat

--- principal.sql
+++ principal.sql
@@ -1,11 +1,17 @@
-SOURCE /home/ubuntu/hospitales/hospitales-data.sql
-SOURCE /home/ubuntu/hospitales/personal-data.sql
-
-SET GLOBAL local_infile = 1;
-
+SOURCE /home/ubuntu/hospitales/hospitales-data.sql;
+SOURCE /home/ubuntu/hospitales/personal-data.sql;
 LOAD DATA LOCAL INFILE '/home/ubuntu/hospitales/personal-data.csv'
 INTO TABLE PERSONAL
-FIELDS TERMINATED BY ';' 
+FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+SOURCE /home/ubuntu/hospitales/administrativo-data.sql;
+SOURCE /home/ubuntu/hospitales/sanitario-data-p1.sql;
+SOURCE /home/ubuntu/hospitales/sanitario-data-p2.sql;
+SOURCE /home/ubuntu/hospitales/medicos-data-p1.sql;
+SOURCE /home/ubuntu/hospitales/medicos-data-p2.sql;
+SOURCE /home/ubuntu/hospitales/enfermeros-data.sql;
+SOURCE /home/ubuntu/hospitales/obrassociales-data.sql;
+SOURCE /home/ubuntu/hospitales/pacientes-data.sql;
+SOURCE /home/ubuntu/hospitales/enfermedades-data.sql;
