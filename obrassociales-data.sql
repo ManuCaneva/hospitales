3047cb0a5998c6804f5910c9300274dc4b75a124
GOYA <francomanuc@gmail.com>
2025-09-18 22:43:17 +0000

    agrego datas

---
+++ obrassociales-data.sql
@@ -0,0 +1,11 @@
+INSERT INTO OBRAS_SOCIALES (nombre) VALUES
+('OSDE'),
+('Swiss Medical'),
+('Medife'),
+('Galeno'),
+('Omint'),
+('Federada Salud'),
+('Sancor Salud'),
+('OSPJN'),
+('IOMA'),
+('PAMI');
